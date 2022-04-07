019376ede58cc3818d6ea958f6afa17c2e681ac7
Hatose <harutomo.seto@gmail.com>
2022-04-08 05:47:56 +0700

    Initial Commit

---
+++ Test Suites/Test 1.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bc545c6f-b3a3-4846-81e9-b3a589849afb</testSuiteGuid>
+   <testCaseLink>
+      <guid>6ff0917e-7757-4038-b8c3-e414a2d1808d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GET list users</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8a2182e7-f195-4706-a26d-78088775b475</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GET single user</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0e8ca0b7-5274-4a97-80e1-8bcd4a39d7f8</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b0d861d9-3a82-4126-b2b4-f5e5521fc5e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PUT update</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5713af5d-3bf3-4907-84e2-619c3d39e3f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POST register successful</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
